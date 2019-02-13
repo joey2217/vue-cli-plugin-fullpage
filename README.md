2c53ec16ff0001ba4d5807018e93eede820f771a
chengfei <chengfei@iuitech.com>
2019-02-13 14:14:52 +0800

    update document

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # vue-cli-plugin-fullpage
-vue-cli-plugin-fullpage
+
+Fullpage.js plugin for `@vue/cli` 3.0.
+
+### Install
+
+First you need to install `@vue/cli` globally (follow the instructions [here](https://cli.vuejs.org/)).
+
+Then create a project and add the fullpage plugin:
+
+```bash
+vue create my-app
+cd my-app
+vue add fullpage
+```
+
+### Use with vue-cli UI
+
+Skip this part if you've done everything in the `Install` section.
+
+If you prefer managing your project in vue-cli UI (by running `vue ui`), here's how you can add fullpage plugin: go to the Plugins menu, click the upper right `+ Add plugin` button, find `fullpage` or `vue-cli-plugin-fullpage` and install it.
+
+### Fullpage.js Doc
+<p>
+  <a href="https://github.com/alvarotrigo/fullPage.js">English</a> |
+  <a href="https://github.com/alvarotrigo/fullPage.js/tree/master/lang/chinese#fullpagejs">中文</a>
+</p>
--- generator/templates/src/App.vue
+++ generator/templates/src/App.vue
@@ -1,32 +1,42 @@
 <template>
   <div>
     <full-page ref="fullpage" :options="options" id="fullpage">
-      <div class="section">First section ...</div>
-      <div class="section">Second section ...</div>
-      <div class="section">Second section ...</div>
+      <div class="section">
+        <h2>Second 1</h2>
+      </div>
+      <div class="section">
+        <h2>Second 2</h2>
+      </div>
+      <div class="section">
+        <h2>Second 3</h2>
+      </div>
     </full-page>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
   components: {},
   data() {
     return {
       options: {
-        menu: '#menu',
-        anchors: ['page1', 'page2', 'page3'],
-        sectionsColor: ['#41b883', '#ff5f45', '#0798ec'],
-      },
+        menu: "#menu",
+        anchors: ["page1", "page2", "page3"],
+        sectionsColor: ["#41b883", "#ff5f45", "#0798ec"]
+      }
     };
-  },
+  }
 };
 </script>
 <style>
-*{
+* {
   margin: 0;
   padding: 0;
+}
+.section{
+  text-align: center;
+  color: aliceblue;
 }
 </style>
 
